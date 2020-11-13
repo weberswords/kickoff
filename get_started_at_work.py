b2d6d21662eff9bd40c4267026e7a12ae44d5b26
Stephanie Weber (Webs) <stephanie.weber@contractor.prometric.com>
2020-11-13 13:12:46 -0800

    add ability to open apps and args for work things

--- get_started_at_work.py
+++ get_started_at_work.py
@@ -5,30 +5,57 @@ import subprocess
 import webbrowser
 from update import run_updates
 
-parser = argparse.ArgumentParser(description='Launch apps and websites or update')
+parser = argparse.ArgumentParser(description='Launch apps and websites or update\n--apps\n--sites\n--update\n--work')
 parser.add_argument('--apps', help='only open the apps', action='store_true')
 parser.add_argument('--sites', help='only open the sites', action='store_true')
 parser.add_argument('--update', help='do updates', action='store_true')
+parser.add_argument('--work', help='also open work-related things', action='store_true')
 
 def launch_sites(sites):
 	for key in sites:
 		print("Opening %s..." % key)
 		webbrowser.open_new_tab(sites[key])
 
-def run(sites):
-	# args = parser.parse_args()
-	run_updates()
-	launch_sites(sites)
+def open_apps(apps):
+	for app in apps:
+		subprocess.call(['open', app])
+
+def run():
+	args = parser.parse_args()
+	if args.update:
+		run_updates()
+	if args.apps:
+	    open_apps(apps)
+	if args.sites:
+	    launch_sites(sites)
+	if args.work:
+		launch_sites(work_sites)
+		open_apps(work_apps)
+	if not args.apps and not args.sites:
+		open_apps(apps)
+		launch_sites(sites)
 
 
 sites = {
 	"Mail" : "https://mail.google.com/",
-	"Github": "https://github.com",
 	"Calendar" : "https://calendar.google.com/",
 	"Drive" : "https://drive.google.com/",
 }
 
-run(sites)
+work_sites = {
+}
+
+apps = [
+	'/Applications/Amphetamine.app',
+	'/Applications/Todoist.app',
+	'/Applications/iTerm.app'
+]
+
+work_apps = [
+	'/Applications/Slack.app'
+]
+
+run()
 
 
 
