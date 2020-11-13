e9b50d491e461ed3bcb4d6ae776f99c421553a05
Stephanie Weber (Webs) <stephanie.weber@contractor.prometric.com>
2020-11-13 11:44:05 -0800

    remove app launch

--- get_started_at_work.py
+++ get_started_at_work.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import argparse
-import psutil
 import subprocess
 import webbrowser
 from update import run_updates
@@ -16,47 +15,20 @@ def launch_sites(sites):
 		print("Opening %s..." % key)
 		webbrowser.open_new_tab(sites[key])
 
-def get_processes():
-	ids = psutil.pids()
-	process_names = []
-	for id in ids:
-		# print("Adding %s" % psutil.Process(id).name())
-		process_names.append(psutil.Process(id).name())
-	return process_names
-
-def launch_apps(apps):
-	process_names = get_processes()
-	for app in apps:
-		if app not in process_names:
-			print("%s not open..." % app)
-			print("Opening %s..." % app)
-			app_name = app + ".app"
-			psutil.Popen(['open', '-ga', app_name])
-		else:
-			print("%s is already open!" % app)
-
-def run(sites, apps):
-	args = parser.parse_args()
-	if args.update:
-		run_updates()
-	if args.apps:
-	    launch_apps(apps)
-	if args.sites:
-	    launch_sites(sites)
-	if not args.apps and not args.sites:
-		launch_apps(apps)
-		launch_sites(sites)
+def run(sites):
+	# args = parser.parse_args()
+	run_updates()
+	launch_sites(sites)
 
 
-apps = ['Slack', '1Password 7', 'Spectacle', 'Amphetamine', 'Docker', 'Todoist']
 sites = {
-"Mail" : "https://mail.google.com/",
-"Github": "https://github.com",
-"Calendar" : "https://calendar.google.com/",
-"Drive" : "https://drive.google.com/",
+	"Mail" : "https://mail.google.com/",
+	"Github": "https://github.com",
+	"Calendar" : "https://calendar.google.com/",
+	"Drive" : "https://drive.google.com/",
 }
 
-run(sites, apps)
+run(sites)
 
 
 
